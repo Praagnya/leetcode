9281bf25111480cb8e43e3fb62cde0b698c87e14
Pragnya Narasimha <53310751+Praagnya@users.noreply.github.com>
2025-10-03 00:13:12 -0700

    Time: 7 ms (57.00%) | Memory: 17.9 MB (54.16%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -2,18 +2,22 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return false
-        mapping_s = {}
-        mapping_t = {}
+        sMap = {}
+        tMap = {}
 
-        # Case 1: No mapping exists in either of the dictionaries
-        for c1, c2 in zip(s, t):
-            if (c1 not in mapping_s) and (c2 not in mapping_t):
-                mapping_s[c1] = c2
-                mapping_t[c2] = c1
+        for i in range(len(s)):
+            sChar = s[i]
+            tChar = t[i]
+            if sChar in sMap: 
+                if sMap[sChar] != tChar: 
+                    return False 
+            else: 
+                sMap[sChar] = tChar
 
-        # Case 2: Either mapping doesn't exist in one of the dictionaries or Mapping exists and it doesn't match in either of the dictionaries or both 
-            elif mapping_s.get(c1) != c2 or mapping_t.get(c2) != c1:
-                return False 
+            if tChar in tMap: 
+                if tMap[tChar] != sChar: 
+                    return False
+            else: 
+                tMap[tChar] = sChar
         
         return True 
-        
