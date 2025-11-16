f47eab672df579c6ba2ec7ff1691841ed90c710c
Pragnya Narasimha <53310751+Praagnya@users.noreply.github.com>
2025-11-15 18:42:03 -0700

    Time: 31 ms (61.97%) | Memory: 20.4 MB (38.53%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,16 +1,36 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        mp = {0: 1}   # prefix sum count map
-        count = 0
-        rsum = 0
+        map = {0: 1} # {running_sum: frequency} allows counting subarray that starts from index 0
+        count = 0 
+        r_sum = 0 
+
+        for i in nums: 
+            r_sum += i
+
+            # check if running sum is in map and keep a count 
+            if r_sum - k in map: 
+                count += map[r_sum - k]
+             
+            # add the running sum to map for further check
+            map[r_sum] = map.get(r_sum, 0) + 1 # map.get will return r_sum if there else o
         
-        for num in nums:
-            rsum += num
-            
-            if rsum - k in mp:
-                count += mp[rsum - k]
-            
-            # update prefix sum frequency
-            mp[rsum] = mp.get(rsum, 0) + 1
+        return count 
         
-        return count
+        """
+        Brute force, we are using nested loops.
+        Time Complexity: 0(n**2)
+        """
+        # count = 0
+        # n = len(nums)
+
+        # for start in range(n): #loop through all the elements
+        #     current_sum = 0
+            
+        #     for end in range(start, n):
+        #         current_sum += nums[end]
+            
+        #         if current_sum == k:
+        #             count += 1
+
+        # return count
+
