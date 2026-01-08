131d5b4fa7f25c362e50229338f10c4731bd88fa
Pragnya Narasimha <53310751+Praagnya@users.noreply.github.com>
2026-01-08 00:22:42 -0700

    Time: 943 ms (29.64%) | Memory: 65.2 MB (5.01%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,28 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # Top Down (Memoization)
+        # Time: O(Coins * Amount)
+        # Space: O(Amount)
+        coins.sort()
+        memo = {0:0}
+
+        def min_coins(amt): 
+            if amt in memo: 
+                return memo[amt]
+            
+            minn = float('inf')
+            for coin in coins: 
+                diff = amt - coin
+                if diff < 0: 
+                    break 
+                minn = min(minn, 1 + min_coins(diff))
+
+            memo[amt] = minn
+            return minn
+
+
+        result = min_coins(amount)
+        if result < float('inf'):
+            return result 
+        else: 
+            return -1 
