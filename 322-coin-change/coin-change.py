5df9ad39bb7a98ad9b3637bc2f371fc73ce5fe29
Pragnya Narasimha <53310751+Praagnya@users.noreply.github.com>
2026-01-09 01:20:20 -0700

    Time: 498 ms (95.13%) | Memory: 19.6 MB (29.63%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,28 +1,47 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort()
+        dp = [0] * (amount + 1)
+        
+        for i in range(1, amount + 1):
+            minn = float('inf')
+
+            for coin in coins: 
+                diff = i - coin 
+                if diff < 0: 
+                    break 
+                minn = min(minn, dp[diff] + 1)
+            
+            dp[i] = minn 
+
+        if dp[amount] < float('inf'):
+            return dp[amount]
+        return -1 
+
+
         # Top Down (Memoization)
         # Time: O(Coins * Amount)
         # Space: O(Amount)
-        coins.sort()
-        memo = {0:0}
+        # coins.sort()
+        # memo = {0:0}
 
-        def min_coins(amt): 
-            if amt in memo: 
-                return memo[amt]
+        # def min_coins(amt): 
+        #     if amt in memo: 
+        #         return memo[amt]
             
-            minn = float('inf')
-            for coin in coins: 
-                diff = amt - coin
-                if diff < 0: 
-                    break 
-                minn = min(minn, 1 + min_coins(diff))
+        #     minn = float('inf')
+        #     for coin in coins: 
+        #         diff = amt - coin
+        #         if diff < 0: 
+        #             break 
+        #         minn = min(minn, 1 + min_coins(diff))
 
-            memo[amt] = minn
-            return minn
+        #     memo[amt] = minn
+        #     return minn
 
 
-        result = min_coins(amount)
-        if result < float('inf'):
-            return result 
-        else: 
-            return -1 
+        # result = min_coins(amount)
+        # if result < float('inf'):
+        #     return result 
+        # else: 
+        #     return -1 
