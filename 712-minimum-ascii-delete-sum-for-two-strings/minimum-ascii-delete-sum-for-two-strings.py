9c2921c57b64813d5741e6a55f23ebf693da67e6
Pragnya Narasimha <53310751+Praagnya@users.noreply.github.com>
2026-01-10 08:18:25 -0700

    Time: 787 ms (17.86%) | Memory: 225.1 MB (5.10%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -1,41 +1,71 @@
-from collections import Counter 
+
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-
-            
-        # Dictionary to store the result of each sub-problem
         saved_result = {}
 
-        # Return minimum cost to make s1[0...i] and s2[0...j] equal
         def compute_cost(i, j):
-
-            # If both strings are empty, then no deletion is required
-            if i < 0 and j < 0:
+            # if both strings are empty
+            if i < 0 and j < 0: 
                 return 0
+        
             
-            # If already computed, then return the result from the dictionary
-            if (i, j) in saved_result:
-                return saved_result[(i, j)]
+            # if already computed 
+            if (i,j) in saved_result: 
+                return saved_result[(i,j)]
             
-            # If any one string is empty, delete all characters of the other string
-            if i < 0:
-                saved_result[(i, j)] = ord(s2[j]) + compute_cost(i, j-1)
-                return saved_result[(i, j)]
-            if j < 0:
-                saved_result[(i, j)] = ord(s1[i]) + compute_cost(i-1, j)
-                return saved_result[(i, j)]
-            
-            # Call sub-problem depending on s1[i] and s2[j]
-            # Save the computed result.
-            if s1[i] == s2[j]:
-                saved_result[(i, j)] = compute_cost(i-1, j-1)
-            else:
-                saved_result[(i, j)] = min(
-                    ord(s1[i]) + compute_cost(i-1, j),
-                    ord(s2[j]) + compute_cost(i, j-1)
-                )
+            # if s1 is empty
+            if i < 0: 
+                saved_result[(i,j)] = ord(s2[j]) + compute_cost(i, j-1)
+                return saved_result[(i,j)]
 
+            # if s2 is empty 
+            if j < 0: 
+                saved_result[(i,j)] = ord(s1[i]) + compute_cost(i-1, j)
+                return saved_result[(i,j)]
+            
+            # if both strings are equal
+            if s1[i] == s2[j]:
+                saved_result[(i,j)] = compute_cost(i-1, j-1)
+
+            else: 
+                saved_result[(i,j)] = min(
+                    ord(s1[i]) + compute_cost(i-1, j), 
+                    ord(s2[j]) + compute_cost(i, j-1)
+                    )
+                
             return saved_result[(i, j)]
 
-        # Return the result of the main problem
-        return compute_cost(len(s1)-1, len(s2)-1)
+        return compute_cost(len(s1) -1, len(s2) -1)
+
+
+
+
+
+
+
+
+        # def compute_cost(i, j):
+        #     # when s1 is empty string, we delete all the characters from s2 
+        #     if i < 0: 
+        #         delete_cost = 0 
+        #         for pointer in range(j+1):
+        #             delete_cost += ord(s2[pointer])
+        #         return delete_cost 
+            
+        #     # when s2 is empty, we delete all the characters from s1
+        #     if j < 0: 
+        #         delete_cost = 0 
+        #         for pointer in range(i + 1):
+        #             delete_cost += ord(s1[pointer])
+        #         return delete_cost 
+            
+        #     # if both are equal 
+        #     if s1[i] == s2[j]: 
+        #         return compute_cost(i-1, j-1)
+        #     else: 
+        #         return min(
+        #             ord(s1[i]) + compute_cost(i-1, j),
+        #             ord(s2[j]) + compute_cost(i, j-1),
+        #             ord(s1[i]) + ord(s2[j]) + compute_cost(i-1, j-1)
+        #         )
+        # return compute_cost(len(s1)-1, len(s2)-1)
